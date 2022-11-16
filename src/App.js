40d5d1f6f98efbe32fab8ecce07c81fc09dc4cd0
Caio Beneton <caio.beneton@gmail.com>
2022-11-15 22:58:56 -0300

    routes added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Add from "./components/Add"
+import Draw from "./components/Draw"
+import Home from "./components/Home"
+import Signin from "./components/Signin"
+import Signup from "./components/Signup"
 import GlobalStyle from "./globalStyles"
 
 export default function App(){
@@ -6,7 +11,11 @@ export default function App(){
         <BrowserRouter>
             <GlobalStyle></GlobalStyle>
             <Routes>
-                <Route path="/"></Route>
+                <Route path="/" element={<Signin></Signin>}></Route>
+                <Route path="/signup" element={<Signup></Signup>}></Route>
+                <Route path="/home" element={<Home></Home>}></Route>
+                <Route path="/add" element={<Add></Add>}></Route>
+                <Route path="/draw" element={<Draw></Draw>}></Route>
             </Routes>
         </BrowserRouter>
     )
